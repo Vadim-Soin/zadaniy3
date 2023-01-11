dc3a7c182db4453642cab18e7968b32c2483e604
Vadim <soinvadikv@mail.ru>
2023-01-12 02:47:35 +0400

    Первое задание сделанно

---
+++ zadaniy01_semenar03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = num / 10000;
+int b = num / 1000 % 10;
+int c = num / 10 % 10;
+int d = num % 10;
+
+if ( a == d & b == c ) Console.WriteLine("Число является палиндромом");
+
+else Console.WriteLine("Число не является палиндромом");
